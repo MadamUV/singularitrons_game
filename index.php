bb2d168a3a14802d85e63e15e00351cb0442ebe7
Erin M. Masterson <erin.margot.masterson@gmail.com>
2014-11-11 07:30:18 -0500

    brings up second page left off on

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# fbpayment: http://ogp.me/ns/fb/fbpayment#">
+		<meta property="og:title" content="Abhi's Bronze Subscription" />
+        <meta property="og:image" content="" />
+        <meta property="og:description" content="" />
+        <meta property="fbpayment:price" content="5.29 USD" />
+        <meta property="fbpayment:alternate_price" content="5.29 EUR" />
+        <meta property="fbpayment:alternate_price" content="5.29 GBP" />
+        <meta property="fbpayment:trial_duration" content="7 days" />
+        <meta property="fbpayment:billing_period" content="1 week" />
+        <meta property="fb:app_id" content="1489562004591644" />
+        <meta property="og:type" content="fbpayment:subscription" />
 <meta charset="UTF-8">
 <title>Singularitrons</title>
         <link rel="stylesheet" href="../style.css" type="text/css" media="screen">
@@ -75,6 +85,34 @@
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
+  function performChat() {
+	$("#sentence_text").keyup(function(){
+		var theSentence = $(this).val();
+		//replacement
+		//use a for loop
+		var iconArray = [["/caution", '<img src="icons/caution.png" alt="caution">'], ["/heart", '<img src="icons/heart.png" alt="heart">'], ["/inside", '<img src="icons/inside.png" alt="inside">'], ["/music", '<img src="icons/musical_note.png" alt="music">'], ["/shamrock", '<img src="icons/shamrock.png" alt="shamrock">'], ["/redo", '<img src="icons/redo.png" alt="redo">'], ["/undo", '<img src="icons/undo.png" alt="undo">'], ["/star", '<img src="icons/star.png" alt="star">'], ["/phone", '<img src="phone/caution.png" alt="phone">'], ["/time", '<img src="icons/waiting.png" alt="time">'], ["/wider", '<img src="icons/wider.png" alt="wider">'], ["/taller", '<img src="icons/taller.png" alt="taller">'], ["/ice", '<img src="icons/Ice.png" alt="ice">'], ["/clouds", '<img src="icons/Overcast.png" alt="clouds">'], ["/rainbow", '<img src="icons/Rainbow.png" alt="rainbow">'], ["/sun", '<img src="icons/Sunny.png" alt="sun">'], ["/fire", '<img src="icons/fire.png" alt="fire">'], ["/afraid", '<img src="icons/afraid.png" alt="afraid">'], ["/happy", '<img src="icons/happy.png" alt="happy">'], ["/delighted", '<img src="icons/delighted.png" alt="delighted">'], ["/disgusted", '<img src="icons/disgusted.png" alt="disgusted">'], ["/angry", '<img src="icons/angry.png" alt="angry">'], ["/confused", '<img src="icons/confused.png" alt="confused">'], ["/bird", '<img src="icons/bird_contour.png" alt="bird">'], ["/bull", '<img src="icons/bull_contour.png" alt="caution">'], ["/cat", '<img src="icons/cat_contour.png" alt="cat">'], ["/cow", '<img src="icons/cow_contour.png" alt="cow">'], ["/duck", '<img src="icons/duck_contour.png" alt="duck">'], ["/elephant", '<img src="icons/elephant_contour.png" alt="elephant">'], ["/fish", '<img src="icons/fish_contour.png" alt="fish">'], ["/horse", '<img src="icons/horse_contour.png" alt="horse">'], ["/ladybug", '<img src="icons/ladybug_contour.png" alt="ladybug">'], ["/leopard", '<img src="icons/leopard_contour.png" alt="leopard">'], ["/lion", '<img src="icons/lion_contour.png" alt="lion">'], ["/zero", '<img src="icons/zero.png" alt="zero">'], ["/one", '<img src="icons/one.png" alt="one">'], ["/two", '<img src="icons/two.png" alt="two">'], ["/three ", '<img src="icons/three.png" alt="three">'], ["/four", '<img src="icons/four.png" alt="four">'], ["/five", '<img src="icons/five.png" alt="five">'], ["/six", '<img src="icons/six.png" alt="six">'], ["/seven", '<img src="icons/seven.png" alt="seven">'], ["/eight", '<img src="icons/eight.png" alt="eight">'], ["/nine", '<img src="icons/nine.png" alt="nine">'], ["/miserable", '<img src="icons/miserable.png" alt="miserable">'], ["/surprised", '<img src= "icons/surprised.png" alt="surprised">'], ["/outside", '<img src="icons/outside.png" alt="outside">'], ["/sad", '<img src="icons/sad.png" alt="sad">']]; //alter theSentence.
+		var i=0;
+		var theSentenceProcessed=theSentence;
+		for(i=0; i<iconArray.length; i++){
+			theSentenceProcessed = theSentenceProcessed.replace(iconArray[i][0], iconArray[i][1]);
+		}
+		$.post("php_post/new_bad_words_script.php", {my_sentence : theSentenceProcessed}, function(bads){
+			$("#sentence").html(bads);
+		});
+		});
+		$("#submitSentence").click(function(){
+			var sentence = $("#sentence").html();
+			$.post("php_post/comb.php", {comb : sentence, me_id : me_id}, function(combing){
+				$("#sentence").html(combing);
+			});
+		});
+  }
+  
+  function toggleEmotes() {
+	$("#emoteToggle").click(function() {
+		$("#emotes").toggle("slow");
+	});
+  }
   // on login success
   function useAPI() {
     FB.api('/me', function(response) {
@@ -83,35 +121,107 @@
 			var apiDiv_content = $("#apiDiv").html();
             $("#buttonDiv").html('Logged in');
             $.post("php_post/checkIdentity.php", {me_id : me_id, me_firstname : me_firstname}, function(data) {
-				//if (apiDiv_content == ""){
-					document.getElementById('callback').innerHTML=data;
-				//}
-			$("#sentence_text").keyup(function(){
-				var theSentence = $(this).val();
-				//replacement
-				//use a for loop
-				var iconArray = [["/caution", '<img src="icons/caution.png" alt="caution">'], ["/heart", '<img src="icons/heart.png" alt="heart">'], ["/inside", '<img src="icons/inside.png" alt="inside">'], ["/music", '<img src="icons/musical_note.png" alt="music">'], ["/shamrock", '<img src="icons/shamrock.png" alt="shamrock">'], ["/redo", '<img src="icons/redo.png" alt="redo">'], ["/undo", '<img src="icons/undo.png" alt="undo">'], ["/star", '<img src="icons/star.png" alt="star">'], ["/phone", '<img src="phone/caution.png" alt="phone">'], ["/time", '<img src="icons/waiting.png" alt="time">'], ["/wider", '<img src="icons/wider.png" alt="wider">'], ["/taller", '<img src="icons/taller.png" alt="taller">'], ["/ice", '<img src="icons/Ice.png" alt="ice">'], ["/clouds", '<img src="icons/Overcast.png" alt="clouds">'], ["/rainbow", '<img src="icons/Rainbow.png" alt="rainbow">'], ["/sun", '<img src="icons/Sunny.png" alt="sun">'], ["/fire", '<img src="icons/fire.png" alt="fire">'], ["/afraid", '<img src="icons/afraid.png" alt="afraid">'], ["/happy", '<img src="icons/happy.png" alt="happy">'], ["/delighted", '<img src="icons/delighted.png" alt="delighted">'], ["/disgusted", '<img src="icons/disgusted.png" alt="disgusted">'], ["/angry", '<img src="icons/angry.png" alt="angry">'], ["/confused", '<img src="icons/confused.png" alt="confused">'], ["/bird", '<img src="icons/bird_contour.png" alt="bird">'], ["/bull", '<img src="icons/bull_contour.png" alt="caution">'], ["/cat", '<img src="icons/cat_contour.png" alt="cat">'], ["/cow", '<img src="icons/cow_contour.png" alt="cow">'], ["/duck", '<img src="icons/duck_contour.png" alt="duck">'], ["/elephant", '<img src="icons/elephant_contour.png" alt="elephant">'], ["/fish", '<img src="icons/fish_contour.png" alt="fish">'], ["/horse", '<img src="icons/horse_contour.png" alt="horse">'], ["/ladybug", '<img src="icons/ladybug_contour.png" alt="ladybug">'], ["/leopard", '<img src="icons/leopard_contour.png" alt="leopard">'], ["/lion", '<img src="icons/lion_contour.png" alt="lion">'], ["/zero", '<img src="icons/zero.png" alt="zero">'], ["/one", '<img src="icons/one.png" alt="one">'], ["/two", '<img src="icons/two.png" alt="two">'], ["/three ", '<img src="icons/three.png" alt="three">'], ["/four", '<img src="icons/four.png" alt="four">'], ["/five", '<img src="icons/five.png" alt="five">'], ["/six", '<img src="icons/six.png" alt="six">'], ["/seven", '<img src="icons/seven.png" alt="seven">'], ["/eight", '<img src="icons/eight.png" alt="eight">'], ["/nine", '<img src="icons/nine.png" alt="nine">'], ["/miserable", '<img src="icons/miserable.png" alt="miserable">'], ["/surprised", '<img src= "icons/surprised.png" alt="surprised">'], ["/outside", '<img src="icons/outside.png" alt="outside">'], ["/sad", '<img src="icons/sad.png" alt="sad">']]; //alter theSentence.
-				var i=0;
-				var theSentenceProcessed=theSentence;
-				for(i=0; i<iconArray.length; i++){
-					theSentenceProcessed = theSentenceProcessed.replace(iconArray[i][0], iconArray[i][1]);
-				}
-				$.post("php_post/new_bad_words_script.php", {my_sentence : theSentenceProcessed}, function(bads){
-					$("#sentence").html(bads);
+				document.getElementById('callback').innerHTML=data;
+				toggleEmotes();
+				$("#select_name").click(function(){
+					var starter_bot = $("#starter_bot_remember").val();
+					var starter_name = $("#robot_name").val();
+					$.post("php_post/check_bot_name.php", {me_id : me_id, starter_name : starter_name}, function(checkData){
+						$("#check_this_name").html(checkData); ///if this warning label isn't empty, nothing will submit to the database
+						if(checkData === "Success!"){
+							$.post("php_post/checkIdentity.php", {me_id : me_id, starter_bot : starter_bot, starter_name : starter_name}, function(bringup){
+								$("#callback").html(bringup);
+								$("#select_habitat").click(function(){
+								var starter_habitat = $("#starter_habitat img").attr("class");
+									$.post("php_post/checkIdentity.php", {me_id : me_id, starter_habitat : starter_habitat}, function(data3) {
+										document.getElementById('callback').innerHTML=data3; //code is repeating; add functions
+										toggleEmotes();
+										performChat();
+									});
+								});
+								$("#next_habitat").click(function(){
+							//
+									if($("#starter_habitat img").attr("class")=="spaceship"){
+										   $("#starter_habitat img").attr("alt", "backyard").attr("class", "backyard").attr("src", "assets_and_scenes/habitat2.jpg").fadeIn();
+										   $("#welcome").html('<p>Backyard lawn suitable for a<br>friendly pet robot at home.</p>');
+									}
+									else if($("#starter_habitat img").attr("class")=="backyard"){
+										   $("#starter_habitat img").attr("alt", "city").attr("class", "city").attr("src", "assets_and_scenes/habitat3.jpg").fadeIn();
+										   $("#welcome").html("<p>View of the city.</p>");
+									}
+									else if($("#starter_habitat img").attr("class")=="city"){
+										   $("#starter_habitat img").attr("alt", "mad scientist lab").attr("class", "lab").attr("src", "assets_and_scenes/habitat4.jpg").fadeIn();
+											$("#welcome").html('<p>Mad science is<br>always fun.</p>');
+									}
+									else {
+										   $("#starter_habitat img").attr("alt", "spaceship").attr("class", "spaceship").attr("src", "assets_and_scenes/habitat1.jpg").fadeIn();
+										   $("#welcome").html('<p>What better place<br>to be as a robot?</p>');
+										   //Note that background images needn't be all one file. They can be layered, and
+										   //while containing perhaps some flash objects for the main bacground, can also
+										   //have some objects further in the foregorund for the robot to interact with.
+									}
+								});
+							});
+						}
+					});
 				});
+				$("#select_habitat").click(function(){
+						var starter_habitat = $("#starter_habitat img").attr("class");
+						$.post("php_post/checkIdentity.php", {me_id : me_id, starter_habitat : starter_habitat}, function(data3) {
+							document.getElementById('callback').innerHTML=data3;
+							toggleEmotes();
+							performChat();
+						});
+					});
+					$("#next_habitat").click(function(){
+				//
+						if($("#starter_habitat img").attr("class")=="spaceship"){
+							   $("#starter_habitat img").attr("alt", "backyard").attr("class", "backyard").attr("src", "assets_and_scenes/habitat2.jpg").fadeIn();
+							   $("#welcome").html('<p>Backyard lawn suitable for a<br>friendly pet robot at home.</p>');
+						}
+						else if($("#starter_habitat img").attr("class")=="backyard"){
+							   $("#starter_habitat img").attr("alt", "city").attr("class", "city").attr("src", "assets_and_scenes/habitat3.jpg").fadeIn();
+							   $("#welcome").html("<p>View of the city.</p>");
+						}
+						else if($("#starter_habitat img").attr("class")=="city"){
+							   $("#starter_habitat img").attr("alt", "mad scientist lab").attr("class", "lab").attr("src", "assets_and_scenes/habitat4.jpg").fadeIn();
+								$("#welcome").html('<p>Mad science is<br>always fun.</p>');
+						}
+						else {
+							   $("#starter_habitat img").attr("alt", "spaceship").attr("class", "spaceship").attr("src", "assets_and_scenes/habitat1.jpg").fadeIn();
+							   $("#welcome").html('<p>What better place<br>to be as a robot?</p>');
+							   //Note that background images needn't be all one file. They can be layered, and
+							   //while containing perhaps some flash objects for the main bacground, can also
+							   //have some objects further in the foregorund for the robot to interact with.
+						}
+					});
+				$("#sentence_text").keyup(function(){
+		var theSentence = $(this).val();
+		//replacement
+		//use a for loop
+		var iconArray = [["/caution", '<img src="icons/caution.png" alt="caution">'], ["/heart", '<img src="icons/heart.png" alt="heart">'], ["/inside", '<img src="icons/inside.png" alt="inside">'], ["/music", '<img src="icons/musical_note.png" alt="music">'], ["/shamrock", '<img src="icons/i.png" alt="shamrock">'], ["/redo", '<img src="icons/redo.png" alt="redo">'], ["/undo", '<img src="icons/undo.png" alt="undo">'], ["/star", '<img src="icons/star.png" alt="star">'], ["/phone", '<img src="phone/caution.png" alt="phone">'], ["/time", '<img src="icons/waiting.png" alt="time">'], ["/wider", '<img src="icons/wider.png" alt="wider">'], ["/taller", '<img src="icons/taller.png" alt="taller">'], ["/ice", '<img src="icons/Ice.png" alt="ice">'], ["/clouds", '<img src="icons/Overcast.png" alt="clouds">'], ["/rainbow", '<img src="icons/Rainbow.png" alt="rainbow">'], ["/sun", '<img src="icons/Sunny.png" alt="sun">'], ["/fire", '<img src="icons/fire.png" alt="fire">'], ["/afraid", '<img src="icons/afraid.png" alt="afraid">'], ["/happy", '<img src="icons/happy.png" alt="happy">'], ["/delighted", '<img src="icons/delighted.png" alt="delighted">'], ["/disgusted", '<img src="icons/disgusted.png" alt="disgusted">'], ["/angry", '<img src="icons/angry.png" alt="angry">'], ["/confused", '<img src="icons/confused.png" alt="confused">'], ["/bird", '<img src="icons/bird_contour.png" alt="bird">'], ["/bull", '<img src="icons/bull_contour.png" alt="caution">'], ["/cat", '<img src="icons/cat_contour.png" alt="cat">'], ["/cow", '<img src="icons/cow_contour.png" alt="cow">'], ["/duck", '<img src="icons/duck_contour.png" alt="duck">'], ["/elephant", '<img src="icons/elephant_contour.png" alt="elephant">'], ["/fish", '<img src="icons/fish_contour.png" alt="fish">'], ["/horse", '<img src="icons/horse_contour.png" alt="horse">'], ["/ladybug", '<img src="icons/ladybug_contour.png" alt="ladybug">'], ["/leopard", '<img src="icons/leopard_contour.png" alt="leopard">'], ["/lion", '<img src="icons/lion_contour.png" alt="lion">'], ["/zero", '<img src="icons/zero.png" alt="zero">'], ["/one", '<img src="icons/one.png" alt="one">'], ["/two", '<img src="icons/two.png" alt="two">'], ["/three ", '<img src="icons/three.png" alt="three">'], ["/four", '<img src="icons/four.png" alt="four">'], ["/five", '<img src="icons/five.png" alt="five">'], ["/six", '<img src="icons/six.png" alt="six">'], ["/seven", '<img src="icons/seven.png" alt="seven">'], ["/eight", '<img src="icons/eight.png" alt="eight">'], ["/nine", '<img src="icons/nine.png" alt="nine">'], ["/miserable", '<img src="icons/miserable.png" alt="miserable">'], ["/surprised", '<img src= "icons/surprised.png" alt="surprised">'], ["/outside", '<img src="icons/outside.png" alt="outside">'], ["/sad", '<img src="icons/sad.png" alt="sad">']]; //alter theSentence.
+		var i=0;
+		var theSentenceProcessed=theSentence;
+		for(i=0; i<iconArray.length; i++){
+			theSentenceProcessed = theSentenceProcessed.replace(iconArray[i][0], iconArray[i][1]);
+		}
+		$.post("php_post/new_bad_words_script.php", {my_sentence : theSentenceProcessed}, function(bads){
+			$("#sentence").html(bads);
+		});
+		});
+		$("#submitSentence").click(function(){
+			var sentence = $("#sentence").html();
+			$.post("php_post/comb.php", {comb : sentence, me_id : me_id}, function(combing){
+				$("#sentence").html(combing);
 			});
-			$("#submitSentence").click(function(){
-				var sentence = $("#sentence").html();
-				$.post("php_post/comb.php", {comb : sentence, me_id : me_id}, function(combing){
-					$("#sentence").html(combing);
-				});
-			});
+		});
 			$("#select_habitat").click(function(){
 						var starter_habitat = $("#starter_habitat img").attr("class");
 						$.post("php_post/checkIdentity.php", {me_id : me_id, starter_habitat : starter_habitat}, function(data3) {
 							document.getElementById('callback').innerHTML=data3;
+							toggleEmotes();
+							performChat();
 						});
-						
 					});
 					$("#next_habitat").click(function(){
 				//
@@ -151,6 +261,8 @@
 								var starter_habitat = $("#starter_habitat img").attr("class");
 									$.post("php_post/checkIdentity.php", {me_id : me_id, starter_habitat : starter_habitat}, function(data3) {
 										document.getElementById('callback').innerHTML=data3; //code is repeating; add functions
+										toggleEmotes();
+										performChat();
 									});
 								});
 								$("#next_habitat").click(function(){
@@ -182,6 +294,7 @@
 				$("#starter_bot_remember").keyup(function(){
 					$("#check_this_name").html(""); //reset the warning label so users have a chance again to type correctly
 				});
+				/*
 				$("#select_habitat").click(function(){
 					var starter_habitat = $("#starter_habitat img").attr("class");
 					$.post("php_post/checkIdentity.php", {me_id : me_id, starter_habitat : starter_habitat}, function(data3) {
@@ -209,7 +322,7 @@
 							   //while containing perhaps some flash objects for the main bacground, can also
 							   //have some objects further in the foregorund for the robot to interact with.
 						}
-					});
+					});*/
 				});
             });
 			//starter bots will need to have separate tables for their ids, or same table perhaps.
