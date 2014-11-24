2c9d43d8bdc259cc4dbc47d69228c11f19b42f1a
Erin M. Masterson <erin.margot.masterson@gmail.com>
2014-11-24 00:09:56 -0500

    update to botid datatable name

--- php_post/check_bot_name.php
+++ php_post/check_bot_name.php
@@ -2,15 +2,15 @@
 	if(isset($_POST['starter_name']) && isset($_POST['me_id'])) {
 		$starter_name = $_POST['starter_name'];
 		include '../php_extras/connect.php';
-		$sql_check = "SELECT * FROM bot_identification";
+		$sql_check = "SELECT * FROM botid";
 		$checking = mysqli_query($conn, $sql_check);
-		if(mysqli_num_rows($checking) !== 0){
+		if(mysqli_num_rows($checking) != 0){
 			echo "Oops, that name is taken. Please try again.";
 		}
 		if(strlen($starter_name)<5 || strlen($starter_name)>12){
 			echo "Oops, name must be within 5 to 12 characters!";
 		}
-		if(strlen($starter_name)>4 && strlen($starter_name)<13 && mysqli_num_rows($checking) === 0){
+		if(strlen($starter_name)>4 && strlen($starter_name)<13 && mysqli_num_rows($checking) == 0){
 			echo "Success!";
 		}
 	}
