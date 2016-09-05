ea27121021d011a1729dddc979b91a81ef3b0d3d
Erin M. Masterson <erin.margot.masterson@gmail.com>
2016-09-05 01:09:14 -0400

    connectivity repairs

--- php_extras/connect.php
+++ php_extras/connect.php
@@ -1,5 +1,5 @@
 <?php
 
-   $conn = mysqli_connect("pdb14.freehostingeu.com", "1650712_singular", "iluvsingular1732", "1650712_singular");
+   $conn = mysqli_connect("fdb5.freehostingeu.com", "1879270_singular", "iluvsingular1732", "1879270_singular");
 
 ?>
